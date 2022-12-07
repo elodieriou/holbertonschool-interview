87f77ba73b2b10c849d93b23e487f022d0de0c8d
Elodie Riou <elodie.riou@hotmail.com>
2022-12-07 10:07:39 +0100

    FIX: initialize the list with 0 and remove the if that verify the value 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,11 +4,9 @@
 
 def canUnlockAll(boxes):
     """This method determines if all the boxes can be opened"""
-    unLockBoxes = []
+    unLockBoxes = [0]
     for box in boxes:
         index = boxes.index(box)
-        if len(box) == 0 and 0 not in unLockBoxes:
-            unLockBoxes.append(0)
         for key in box:
             if key != index and key < len(boxes) and key not in unLockBoxes:
                 unLockBoxes.append(key)
