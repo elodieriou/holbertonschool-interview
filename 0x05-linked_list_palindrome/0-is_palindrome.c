ea438073a274a80e638b8899f6c62626731beaba
Elodie Riou <elodie.riou@hotmail.com>
2023-01-17 09:55:32 +0100

    FEATURE: implement a function that check if a linked list is a palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,99 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - function that check if a linked list is a palindrome
+ * @head: the beginning of the linked list
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow, *fast, *end = NULL, *newHead = NULL;
+	int len;
+
+	if (*head == NULL || head == NULL)
+		return (1);
+
+	len = size_listint(&*head);
+	if (len == 1)
+		return (1);
+
+	slow = fast = *head;
+	while (fast != NULL && fast->next != NULL)
+	{
+		end = slow;
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (fast != NULL)
+			newHead = slow->next;
+		else
+			newHead = slow;
+	}
+	end->next = NULL;
+
+	if (len > 3)
+		reverse_listint(&newHead);
+
+	return (compare_listint(*head, &*newHead));
+}
+
+/**
+ * size_listint - function that define the number of node in a linked list
+ * @head: the beginning of the linked list
+ * Return: the number of node
+ */
+int size_listint(listint_t **head)
+{
+	listint_t *tmp = *head;
+	int len = 0;
+
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+	return (len);
+}
+
+/**
+ * reverse_listint - function that reverse a linked list
+ * @head: the beginning of the linked list
+ * Return: Nothing
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL, *tmp;
+
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		tmp->next = prev;
+		prev = tmp;
+		tmp = next;
+	}
+	*head = prev;
+}
+
+/**
+ * compare_listint - function that check if two linked list are identical
+ * @head1: the beginning of the first linked list
+ * @head2: the beginning of the second linked list
+ * Return: true if the lists are identical, false otherwise
+ */
+bool compare_listint(listint_t *head1, listint_t *head2)
+{
+	listint_t *tmp1 = head1, *tmp2 = head2;
+
+	while (tmp1 != NULL && tmp2 != NULL)
+	{
+		if (tmp1->n == tmp2->n)
+		{
+			tmp1 = tmp1->next;
+			tmp2 = tmp2->next;
+		}
+		else
+			return (false);
+	}
+	return (true);
+}
