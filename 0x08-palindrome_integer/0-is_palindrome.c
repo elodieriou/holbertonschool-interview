5a1fc97f6676ffe986b94585aaf06e2110191a72
Elodie Riou <elodie.riou@hotmail.com>
2023-02-07 11:45:35 +0100

    FEATURE: implement function that check if an integer is a palindrome

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - function that check if an integer is a palindrome
+ * @n: the number checked
+ * Return: 1 if n is a palindrome, 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long reminder, reversed = 0, num = n;
+
+	while (num != 0)
+	{
+		reminder = num % 10;
+		reversed = reversed * 10 + reminder;
+		num = num / 10;
+	}
+
+	if (reversed == n)
+		return (1);
+	return (0);
+}
