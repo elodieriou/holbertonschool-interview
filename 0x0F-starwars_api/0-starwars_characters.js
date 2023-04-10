b7d0378f4d9d001c7ffcf860953c20e30b0ae6e3
Elodie Riou <elodie.riou@hotmail.com>
2023-04-10 13:27:33 +0200

    FEATURE: get the star wars api using promises

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request.get(url, async (error, response, body) => {
+    const film = JSON.parse(body)
+
+    for (const character of film.characters) {
+        await new Promise((resolve) => {
+            request.get(character, (error, response, body) => {
+                const character = JSON.parse(body)
+                console.log(character.name);
+                resolve();
+            });
+        });
+    }
+});
