83522c918448d6747ab94fa6199579711c516c1b
Elodie Riou <elodie.riou@hotmail.com>
2023-04-13 08:14:52 +0200

    FIX: semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,16 +4,16 @@ const request = require('request');
 
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-request.get(url, async (error, response, body) => {
-    const film = JSON.parse(body)
+request.get(url, async (response, body) => {
+  const film = JSON.parse(body);
 
-    for (const character of film.characters) {
-        await new Promise((resolve) => {
-            request.get(character, (error, response, body) => {
-                const character = JSON.parse(body)
-                console.log(character.name);
-                resolve();
-            });
-        });
-    }
+  for (const character of film.characters) {
+    await new Promise((resolve) => {
+      request.get(character, (response, body) => {
+        const character = JSON.parse(body);
+        console.log(character.name);
+        resolve();
+      });
+    });
+  }
 });
