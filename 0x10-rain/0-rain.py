a95864cafc13621d241cf3d0f0cbaae0b63b388b
Elodie Riou <elodie.riou@hotmail.com>
2023-04-17 11:14:01 +0200

    FEATURE: calculate how many square units of water will be retained after it rains

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""rain module"""
+
+
+def rain(walls):
+    """Rain function calculate how many square of water will be retained
+    after it rains.
+    """
+
+    if len(walls) == 0:
+        return 0
+
+    area = 0
+    length = len(walls)
+
+    for i in range(length):
+        if walls[i] == 0:
+            continue
+        count = 0
+
+        for j in range(i + 1, length):
+            if walls[j] != 0 or j == length - 1:
+                break
+            count += 1
+
+        if i < length - 1:
+            min_wall = min(walls[i], walls[i + count + 1])
+            area += count * min_wall
+
+    return area
