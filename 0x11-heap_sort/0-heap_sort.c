9964e9e3ee9e52cc7cc1e699d7d66a34afd189a1
Elodie Riou <elodie.riou@hotmail.com>
2023-04-27 09:33:57 +0200

    FIX: betty

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -50,12 +50,12 @@ void heapify(int *array, int n, int i, size_t size)
 	int left = 2 * i + 1;
 	int right = 2 * i + 2;
 
-	if(left < n && array[left] > array[largest])
+	if (left < n && array[left] > array[largest])
 		largest = left;
-	if(right < n && array[right] > array[largest])
+	if (right < n && array[right] > array[largest])
 		largest = right;
 
-	if(largest != i)
+	if (largest != i)
 	{
 		swap(&array[i], &array[largest]);
 		print_array(array, size);
@@ -74,10 +74,10 @@ void heap_sort(int *array, size_t size)
 {
 	int i;
 	/* Build max heap */
-	for(i = size / 2 - 1; i >= 0; i--)
+	for (i = size / 2 - 1; i >= 0; i--)
 		heapify(array, size, i, size);
 
-	for(i = size - 1; i > 0; i--)
+	for (i = size - 1; i > 0; i--)
 	{
 		swap(&array[0], &array[i]);
 		print_array(array, size);
