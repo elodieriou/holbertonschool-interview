5b953b423f70e589c6db284ba00c869a9daa485a
Elodie Riou <elodie.riou@hotmail.com>
2023-05-23 18:40:30 +0200

    FEATURE: implement a function that computes the product of infinite numbers

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,106 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - function that checks if the argument is a number
+ * @c: the argument
+ * Return: 1 if is a number, 0 otherwise
+ */
+int _isdigit(char *c)
+{
+	while (*c)
+	{
+		if (*c < '0' || *c > '9')
+			return (0);
+		c++;
+	}
+
+	return (1);
+}
+
+/**
+ * print_error - function that print the error message
+ * Return: Nothing
+ */
+void print_error(void)
+{
+	int i;
+	char *str = "Error\n";
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+}
+
+/**
+ * _strlen - function that calculates the length of a string
+ * @str: the string
+ * Return: the length
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * multiply - function that computes the product of infinite numbers
+ * @str1: the argument with number 1
+ * @str2: the argument with number 2
+ * Return: Nothing
+ */
+
+void multiply(char *str1, char *str2)
+{
+	int i, j, num1, num2, carry;
+	int len1 = _strlen(str1);
+	int len2 = _strlen(str2);
+	int total = len1 + len2;
+	int *res = malloc(sizeof(int) * total);
+
+	if (res == NULL)
+		return;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		num1 = str1[i] - '0';
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			num2 = str2[j] - '0';
+			res[i + j] += num1 * num2 + carry;
+			carry = res[i + j] / 10;
+			res[i + j] %= 10;
+		}
+		if (carry > 0)
+			res[i + j] %= 10;
+	}
+
+	for (i = 0; i < total - 1; i++)
+		_putchar(res[i] + '0');
+	_putchar('\n');
+}
+
+/**
+ * main - Entry point
+ * @argc: the number of arguments
+ * @argv: the value of arguments
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	char *num1 = argv[1];
+	char *num2 = argv[2];
+
+	if (argc != 3 || _isdigit(num1) == 0 || _isdigit(num2) == 0)
+	{
+		print_error();
+		exit(98);
+	}
+
+	multiply(num1, num2);
+	return (0);
+}
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int _strlen(char *str);
+int _isdigit(char *c);
+void print_error(void);
+void multiply(char *str1, char *str2);
+
+#endif /* HOLBERTON_H */
